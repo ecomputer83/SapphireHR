e20076d74f010946ac5e0c8e41489b2f1ce47016
Giwa Uthman <giwauthman8@gmail.com>
2021-02-11 12:29:25 -0500

    Update Contoller

--- SapphireHR.Business.Abstractions/Service/IApplicationService.cs
+++ SapphireHR.Business.Abstractions/Service/IApplicationService.cs
@@ -10,23 +10,23 @@ namespace SapphireHR.Business.Abstractions.Service
     {
         Task AddApplicationScore(ApplicationScoreModel model);
         Task UpdateApplicationScore(ApplicationScoreModel model, int id);
-        Task ReadApplicationScore(int id);
+        Task<ApplicationScoreModel> ReadApplicationScore(int id);
         Task RemoveApplicationScore(int id);
         Task AddApplicationFaceToView(ApplicationFaceToViewModel model);
         Task UpdateApplicationFaceToView(ApplicationFaceToViewModel model, int id);
-        Task ReadApplicationFaceToView(int id);
+        Task<ApplicationFaceToViewModel> ReadApplicationFaceToView(int id);
         Task RemoveApplicationFaceToView(int id);
         Task AddApplicationInterview(ApplicationInterviewModel model);
         Task UpdateApplicationInterview(ApplicationInterviewModel model, int id);
-        Task ReadApplicationInterview(int id);
+        Task<ApplicationInterviewModel> ReadApplicationInterview(int id);
         Task RemoveApplicationInterview(int id);
         Task AddApplicationSkills(ApplicationSkillModel model);
         Task UpdateAddApplicationSkills(ApplicationSkillModel model, int id);
-        Task ReadApplicationSkills(int id);
+        Task<ApplicationSkillModel> ReadApplicationSkills(int id);
         Task RemoveApplicationSkills(int id);
         Task AddApplicationNegotiation(ApplicationNegotiationModel model);
         Task UpdateApplicationNegotiation(ApplicationNegotiationModel model, int id);
-        Task ReadApplicationNegotiation(int id);
+        Task<ApplicationNegotiationModel> ReadApplicationNegotiation(int id);
         Task RemoveApplicationNegotiation(int id);
 
 
--- SapphireHR/Controllers/ApplicationController.cs
+++ SapphireHR/Controllers/ApplicationController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SapphireHR.Business.Abstractions.Models;
 using SapphireHR.Business.Abstractions.Service;
 using System;
 using System.Collections.Generic;
@@ -11,7 +13,7 @@ namespace SapphireHR.Web.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class ApplicationController : ControllerBase
+    public class ApplicationController : BaseApiController
     {
         IApplicationService _applicationService;
         private readonly ILogger<ApplicationController> _logger;
@@ -21,35 +23,325 @@ namespace SapphireHR.Web.Controllers
             _applicationService = applicationService;
             _logger = logger;
         }
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/<ValuesController>/5
+        [Authorize(Roles = "Administrator")]
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetApplicationScore(int id)
         {
-            return "value";
+            try
+            {
+                var resource = await _applicationService.ReadApplicationScore(id);
+                return Ok(resource);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
         }
 
-        // POST api/<ValuesController>
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetApplicationFaceToView(int id)
+        {
+            try
+            {
+                var resource = await _applicationService.ReadApplicationFaceToView(id);
+                return Ok(resource);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetApplicationInterview(int id)
+        {
+            try
+            {
+                var resource = await _applicationService.ReadApplicationInterview(id);
+                return Ok(resource);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetApplicationSkill(int id)
+        {
+            try
+            {
+                var resource = await _applicationService.ReadApplicationSkills(id);
+                return Ok(resource);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetApplicationNegotiation(int id)
+        {
+            try
+            {
+                var resource = await _applicationService.ReadApplicationNegotiation(id);
+                return Ok(resource);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+        [Authorize(Roles = "Administrator")]
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> PostApplicationScore([FromBody] ApplicationScoreModel payload)
         {
+            try
+            {
+                await _applicationService.AddApplicationScore(payload);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
         }
 
-        // PUT api/<ValuesController>/5
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        public async Task<IActionResult> PostApplicationFaceToView([FromBody] ApplicationFaceToViewModel payload)
+        {
+            try
+            {
+                await _applicationService.AddApplicationFaceToView(payload);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        public async Task<IActionResult> PostApplicationInterview([FromBody] ApplicationInterviewModel payload)
+        {
+            try
+            {
+                await _applicationService.AddApplicationInterview(payload);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        public async Task<IActionResult> PostApplicationSkills([FromBody] ApplicationSkillModel payload)
+        {
+            try
+            {
+                await _applicationService.AddApplicationSkills(payload);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost]
+        public async Task<IActionResult> PostApplicationNegotiation([FromBody] ApplicationNegotiationModel payload)
+        {
+            try
+            {
+                await _applicationService.AddApplicationNegotiation(payload);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> PutApplicationScore(int id, [FromBody] ApplicationScoreModel payload)
         {
+            try
+            {
+                await _applicationService.UpdateApplicationScore(payload, id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
         }
 
-        // DELETE api/<ValuesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplicationFaceToView(int id, [FromBody] ApplicationFaceToViewModel payload)
         {
+            try
+            {
+                await _applicationService.UpdateApplicationFaceToView(payload, id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplicationInterview(int id, [FromBody] ApplicationInterviewModel payload)
+        {
+            try
+            {
+                await _applicationService.UpdateApplicationInterview(payload, id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplicationSkills(int id, [FromBody] ApplicationSkillModel payload)
+        {
+            try
+            {
+                await _applicationService.UpdateAddApplicationSkills(payload, id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplicationNegotiation(int id, [FromBody] ApplicationNegotiationModel payload)
+        {
+            try
+            {
+                await _applicationService.UpdateApplicationNegotiation(payload, id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplicationScore(int id)
+        {
+            try
+            {
+                await _applicationService.RemoveApplicationScore( id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplicationNegotiation(int id)
+        {
+            try
+            {
+                await _applicationService.RemoveApplicationNegotiation(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplicationSkills(int id)
+        {
+            try
+            {
+                await _applicationService.RemoveApplicationSkills(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplicationInterview(int id)
+        {
+            try
+            {
+                await _applicationService.RemoveApplicationInterview(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplicationFaceToView(int id)
+        {
+            try
+            {
+                await _applicationService.RemoveApplicationFaceToView(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return CreateApiException(ex);
+            }
         }
     }
 }
