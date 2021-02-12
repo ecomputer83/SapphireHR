3be421bf608b591fcf7fccc16aecadf024a56cd6
Bakare Biola S <ecomputer83@gmail.com>
2021-02-12 14:57:36 +0100

    Configuration change on Account Controller

--- SapphireHR/Controllers/AccountController.cs
+++ SapphireHR/Controllers/AccountController.cs
@@ -1,8 +1,8 @@
 ﻿using AutoMapper;
-using AutoMapper.Configuration;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
