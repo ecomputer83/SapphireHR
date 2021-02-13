8869968194e6209f20a7754d37b2d1626768f685
Bakare Biola S <ecomputer83@gmail.com>
2021-02-13 11:09:58 +0100

    modified

---
+++ SapphireHR.Business.Abstractions/Models/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SapphireHR.Business.Abstractions.Models
+{
+    public class UserViewModel
+    {
+        public int OrganizationId { get; set; }
+        public string FullName { get; set; }
+        public int UserType { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
