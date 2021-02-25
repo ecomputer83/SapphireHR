cbf96b118a925905750376854f42dcfdcc412a52
Bakare Biola S <ecomputer83@gmail.com>
2021-02-25 19:05:29 +0100

    model modified

--- SapphireHR.Business.Abstractions/Models/VacancyModel.cs
+++ SapphireHR.Business.Abstractions/Models/VacancyModel.cs
@@ -12,5 +12,16 @@ namespace SapphireHR.Business.Abstractions.Models
         public int Quantity { get; set; }
         public string Description { get; set; }
         public string Scores { get; set; }
+        public int RequestedBy { get; set; }
+        public DateTime RequestedOn { get; set; }
+        public DateTime PeriodFrom { get; set; }
+        public DateTime PeriodTo { get; set; }
+
+        public EmployeeModel Requestor { get; set; }
+
+        public DesignationModel Designation { get; set; }
+        public JobProfileModel JobProfile { get; set; }
+
+        public virtual JobRequisitionModel JobRequisition { get; set; }
     }
 }
