fb20d71f267f90660035d3d44f70f96d8416fbcc
Bakare Biola S <ecomputer83@gmail.com>
2021-03-16 19:10:12 +0100

    Expenseclaim modified

--- SapphireHR.Business.Abstractions/Models/ExpenseClaimModel.cs
+++ SapphireHR.Business.Abstractions/Models/ExpenseClaimModel.cs
@@ -6,13 +6,12 @@ namespace SapphireHR.Business.Abstractions.Models
 {
     public class ExpenseClaimModel
     {
-        public DateTimeOffset Date { get; set; }
+        public DateTimeOffset? Date { get; set; }
         public string Remark { get; set; }
         public int Amount { get; set; }
         public int Assignee { get; set; }
         public int Approval { get; set; }
-        public DateTimeOffset ApprovalDate { get; set; }
-        public EmployeeModel ApprovalEmployee { get; set; }
+        public DateTimeOffset? ApprovalDate { get; set; }
         public EmployeeModel AssigneeEmployee { get; set; }
     }
 }
