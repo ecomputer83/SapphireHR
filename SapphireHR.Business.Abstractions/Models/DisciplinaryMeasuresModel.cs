7749f3ae02237f8aa4947cc228ada8e4b10ee0fc
ovata <ovataworld@gmail.com>
2021-04-09 12:23:48 +0100

    commit

--- SapphireHR.Business.Abstractions/Models/DisciplinaryMeasuresModel.cs
+++ SapphireHR.Business.Abstractions/Models/DisciplinaryMeasuresModel.cs
@@ -6,6 +6,7 @@ namespace SapphireHR.Business.Abstractions.Models
 {
     public class DisciplinaryMeasuresModel
     {
+        public int Id { get; set; }
         public DateTime Date { get; set; }
         public string HRManager { get; set; }
         public string Reason { get; set; }
